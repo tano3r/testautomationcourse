90192a5abc457a1678dd9b7ca71b080e03f4e485
Wiktor <pagacz.wiktor@wp.pl>
2020-07-21 00:37:02 +0200

    Created classes for login scenario

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,26 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.firefox.driver", "resources/geckodriver");
         driver = new FirefoxDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        WebElement inputsLink =  driver.findElement(By.linkText("Shifting Content"));
-        inputsLink.click();
-
-        WebElement exampleOne = driver.findElement(By.partialLinkText("Example 1"));
-        exampleOne.click();
-
-        List<WebElement> buttonMenuList = driver.findElements(By.tagName("li"));
-        System.out.println(buttonMenuList.size());
+        homePage = new HomePage(driver);
 
         driver.quit();
     }
