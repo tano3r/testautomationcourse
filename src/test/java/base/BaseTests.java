6631b842a6224f64fdfd084818f53cf3692bba11
Wiktor <pagacz.wiktor@wp.pl>
2020-07-23 01:03:45 +0200

    Created successful login test, add testng to project

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 import java.util.List;
@@ -13,18 +15,19 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    //This method will run before every testclass
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.firefox.driver", "resources/geckodriver");
         driver = new FirefoxDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         homePage = new HomePage(driver);
-
-        driver.quit();
     }
 
-    public static void main(String[] args){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    //This method will run after every testclass
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
